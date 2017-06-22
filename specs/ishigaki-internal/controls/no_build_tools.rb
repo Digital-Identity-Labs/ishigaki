a18a31dfaee97efb0cde3c0abe6f674ed8ff6dff
Pete Birkinshaw <pete@binary-ape.org>
2017-06-22 11:43:47 +0100

    Whitespace?

--- specs/ishigaki-internal/controls/no_build_tools.rb
+++ specs/ishigaki-internal/controls/no_build_tools.rb
@@ -4,7 +4,7 @@ control "no_build_tools" do
   desc "It's a little harder to build malware on a compromised container"
 
 
-  %w[gcc make libc6-dev].each do |deb| 
+  %w[gcc make libc6-dev].each do |deb|
 
     describe package(deb) do
       it { should_not be_installed }
