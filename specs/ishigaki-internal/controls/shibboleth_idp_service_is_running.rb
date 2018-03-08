6ecb9b36798ecfb82093e30c41d35ffa2c90d25b
Pete Birkinshaw <pete@binary-ape.org>
2018-03-08 15:12:32 +0000

    Fix for local timeouts (Travis was fine)

--- specs/ishigaki-internal/controls/shibboleth_idp_service_is_running.rb
+++ specs/ishigaki-internal/controls/shibboleth_idp_service_is_running.rb
@@ -3,7 +3,7 @@ control "shibboleth_idp_service_is_running" do
   title "The Shibboleth IdP service is running"
   desc "The Shibboleth IdP service is running on this container"
 
-  describe http('http://localhost:8080/idp/status') do
+  describe http('http://localhost:8080/idp/status', open_timeout: 90) do
     its('status') { should cmp 200 }
     its('body') { should include 'idp_version: 3.' }
   end
